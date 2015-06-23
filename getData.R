21fc0a227a3df8747827c7167dedd7e157de5f32
Mathurin Dorel <dorel@clipper.ens.fr>
2015-06-23 15:03:59 +0200

    Fix libraries path for getData.R

--- getData.R
+++ getData.R
@@ -1,3 +1,8 @@
+#!/usr/bin/Rscript
+#-*- coding:utf8 -*-
+options(width=1000)
+.libPaths("/home/ubuntu/R/x86_64-unknown-linux-gnu-library/3.0/")
+
 library(cBioFetchR)
 
 study_id = commandArgs(trailingOnly=T)
